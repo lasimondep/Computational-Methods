3c4383f174e76069e76aa0321c9c90cfab0a2340
lasimondep <lasimondep@inbox.ru>
2021-03-25 18:53:28 +0300

    Complete 7th task

--- 7_ProjODE.py
+++ 7_ProjODE.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 from scipy.integrate import quad
-
+import math
 import common.__plots as plt
 
 
@@ -37,8 +37,7 @@ def u(phi, c, x):
         res += it(x, n) * c[n]
     return res
 
-def Galerkin(p, q, r, f, a, b, alpha, beta):
-    N = 4
+def Galerkin(p, q, r, f, a, b, alpha, beta, N):
     phi = []
     phiDeriv = []
     phiDeriv2 = []
@@ -55,12 +54,36 @@ def Galerkin(p, q, r, f, a, b, alpha, beta):
         for j in range(N):
             A[i, j] = quad(lambda x, i, j: (p(x) * phiDeriv2[j](x, j) + q(x) * phiDeriv[j](x, j) + r(x) * phi[j](x, j)) * phi[i](x, i), a, b, args=(i, j))[0]
 
-    print('A:', A)
-    print('d:', d)
     c = np.linalg.solve(A, d)
-    print('c:', c)
     return lambda x: u(phi, c, x)
 
+def Colocation(p, q, r, f, a, b, alpha, beta, N):
+    t = np.array([np.cos((2 * k - 1) * np.pi / (2 * N)) for k in range(1, N + 1)]) # В качестве узлов колокации --- корни многочлена Чебышева
+	
+    f = f(t)
+    phi = []
+    phiDeriv = []
+    phiDeriv2 = []
+
+    A = np.matrix(np.zeros((N, N)))
+    
+    for i in range(N):
+        phi.append(lambda x, i: (1 - x * x) * PolyJac(1, i, x))
+        phiDeriv.append(lambda x, i: PolyJacPhiDeriv(1, i, x))
+        phiDeriv2.append(lambda x, i: PolyJacPhiDeriv2(1, i, x))
+    
+    for j in range(N):
+        for i in range(N):
+            A[j, i] = q(t[j]) * phiDeriv[i](t[j], i) + p(t[j])*phiDeriv2[i](t[j], i) + r(t[j]) * phi[i](t[j], i) * (1 - t[j] * t[j])
+    
+    c = np.linalg.solve(A, f)
+    return lambda x: u(phi, c, x)
+
+def Richardson(u, u_prev, p=2):
+    # Погрешности по правилу Рунге
+    t = np.linspace(-1, 1)
+    delta = np.linalg.norm((u(t)  - u_prev(t)) * (2 ** p) / (2 ** p - 1))
+    return delta
 
 class ODE:
     """
@@ -70,7 +93,7 @@ class ODE:
     alpha[0]u(a) - alpha[1]u'(a) = alpha[2]
     beta[0]u(b) +  beta[1]u'(b) =  beta[2]
     """
-    def __init__(self, msg, p, q, r, f, a, b, alpha, beta):
+    def __init__(self, msg, p, q, r, f, a, b, alpha, beta, N):
         self.msg = msg
         self.p = p
         self.q = q
@@ -80,81 +103,71 @@ class ODE:
         self.b = b
         self.alpha = alpha
         self.beta = beta
-
-
-def Richardson(ode, p=2, N=10, eps=1e-5):
-    DeltaL2 = []
-    _N = [N,]
-    x_prev, u_prev = GridFDM(ode.p, ode.q, ode.r, ode.f, ode.a, ode.b, ode.alpha, ode.beta, N)
-    while N < 1e4:
-        N *= 2 # Сгущение сетки
-        print('N =', N)
-        x, u = GridFDM(ode.p, ode.q, ode.r, ode.f, ode.a, ode.b, ode.alpha, ode.beta, N)
-
-        # Погрешности по правилу Рунге
-        delta = ((u[1:-1:2] + u[2::2]) / 2 - u_prev[1:-1]) * (2 ** p) / (2 ** p - 1)
-        DeltaL2.append(np.sqrt((delta ** 2).sum() / delta.shape[0]))
-        print('|Δ| =', DeltaL2[-1])
-
-        # Проверяем достижение заданной точности
-        if eps > DeltaL2[-1]:
-            u_prev[1:-1] += delta
-            break
-        x_prev, u_prev = x, u
-        _N.append(N)
-
-    return x_prev, u_prev, _N, DeltaL2
-
+        self.N = N
 
 test_case = [
-    #(ODE('Вариант 3',
-    #    p=lambda x: -1 / (x - 3),
-    #    q=lambda x: 1 + x / 2,
-    #    r=lambda x: np.exp(x / 2),
-    #    f=lambda x: 2 - x,
-    #    a=-1, b=1,
-    #    alpha=(1, 0, 0), beta=(1, 0, 0)),
-    #1e-5),
-    #(ODE('Вариант 6',
-    #    p=lambda x: (x - 2) / (x + 2),
-    #    q=lambda x: x,
-    #    r=lambda x: 1 - np.sin(x),
-    #    f=lambda x: x * x,
-    #    a=-1, b=1,
-    #    alpha=(1, 0, 0), beta=(1, 0, 0)),
-    #1e-7),
+    (ODE('Вариант 3',
+        p=lambda x: -1 / (x - 3),
+        q=lambda x: 1 + x / 2,
+        r=lambda x: np.exp(x / 2),
+        f=lambda x: 2 - x,
+        a=-1, b=1,
+        alpha=(1, 0, 0), beta=(1, 0, 0),
+        N=[4, 6, 8, 10]),
+    1e-5),
+    (ODE('Вариант 6',
+        p=lambda x: (x - 2) / (x + 2),
+        q=lambda x: x,
+        r=lambda x: 1 - np.sin(x),
+        f=lambda x: x * x,
+        a=-1, b=1,
+        alpha=(1, 0, 0), beta=(1, 0, 0),
+        N=[4, 6, 8, 10]),
+    1e-7),
     (ODE('Вариант 8',
         p=lambda x: -(4 - x) / (5 - 2 * x),
         q=lambda x: (1 - x) / 2,
         r=lambda x: np.log(3 + x) / 2,
         f=lambda x: 1 + x / 3,
         a=-1, b=1,
-        alpha=(1, 0, 0), beta=(1, 0, 0)),
+        alpha=(1, 0, 0), beta=(1, 0, 0),
+        N=[4, 6, 8, 10]),
     1e-8),
-    #(ODE('Вариант 16',
-    #    p=lambda x: 1 + 0 * x,
-    #    q=lambda x: -np.cos(x) / (1 + x),
-    #    r=lambda x: 2 - x,
-    #    f=lambda x: x + 1,
-    #    a=0, b=1,
-    #    alpha=(0.2, -1, -0.8), beta=(0.9, 1, -0.1)),
-    #1e-8),
+    (ODE('Вариант 16',
+        p=lambda x: 1 + 0 * x,
+        q=lambda x: -np.cos(x) / (2 + x),
+        r=lambda x: 2 - x,
+        f=lambda x: x + 1,
+        a=-1, b=1,
+        alpha=(0.2, -1, -0.8), beta=(0.9, 1, -0.1),
+        N=[4, 6, 8, 10]),
+    1e-8),
 ]
 
 for ode, eps in test_case:
     print(ode.msg)
-    _u = Galerkin(ode.p, ode.q, ode.r, ode.f, ode.a, ode.b, ode.alpha, ode.beta)
+    m = len(ode.N)
+    uGalerkin = []
+    uGalerkin.append(Galerkin(ode.p, ode.q, ode.r, ode.f, ode.a, ode.b, ode.alpha, ode.beta, 2))
+    delta1 = np.zeros(m)
+    uColocation = []
+    uColocation.append(Colocation(ode.p, ode.q, ode.r, ode.f, ode.a, ode.b, ode.alpha, ode.beta, 2))
+    delta2 = np.zeros(m)
+    for i in range(m):
+         uGalerkin.append(Galerkin(ode.p, ode.q, ode.r, ode.f, ode.a, ode.b, ode.alpha, ode.beta, ode.N[i]))
+         delta1[i] = Richardson(uGalerkin[i + 1], uGalerkin[i])
+         uColocation.append(Colocation(ode.p, ode.q, ode.r, ode.f, ode.a, ode.b, ode.alpha, ode.beta, ode.N[i]))
+         delta2[i] = Richardson(uColocation[i + 1], uColocation[i])
     _x = np.linspace(-1, 1)
-    plot_xu = plt.Plot2D((r'$x$', r'$u$'))
-    plot_xu.plot(_x, _u(_x), '-')
-    plot_xu.show()
+    plot_xuGalerkin = plt.Plot2D((r'$x$', r'$u$'))
+    for i in range(m):
+    	plot_xuGalerkin.plot(_x, uGalerkin[i + 1](_x), '-', label="%d nodes" % (ode.N[i]))
 
-    #x, u, N, DeltaL2 = Richardson(ode, eps=eps)
+    plot_xuColocation = plt.Plot2D((r'$x$', r'$u$'))
+    for i in range(m):
+    	plot_xuColocation.plot(_x, uColocation[i + 1](_x), '-', label="%d nodes" % (ode.N[i]))
 
-    #plot_xu = plt.Plot2D((r'$x$', r'$u$'))
-    #plot_NDelta = plt.Plot2D((r'$N$', r'$\Delta$'), (False, True))
-
-    #plot_xu.plot(x, u, '-')
-    #plot_NDelta.plot(N, DeltaL2, '-')
-
-    #plot_xu.show()
+    plot_Delta = plt.Plot2D((r'$x$', r'\Delta'), (False, True))
+    plot_Delta.plot(ode.N, delta1, '-', label="Galerkin")
+    plot_Delta.plot(ode.N, delta2, '-', label="Colocation")
+    plot_Delta.show()
